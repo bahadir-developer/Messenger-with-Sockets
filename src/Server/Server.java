499c58b1e20fe9ae751057e95b81a30260319efe
Bahadir-coder <bahadurmusayev15@gmail.com>
2026-03-19 04:18:08 +0400

    Server Layer Update

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,7 +1,20 @@
 package Server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 public class Server {
-    Socket socket = new Socket();
+    public static void main(String[] args) throws IOException {
+        Socket s = null;
+        ServerSocket socket = new ServerSocket(9000);
+        s = socket.accept();
+
+        InputStream inputStream = new Inp
+        if(s != null){
+
+        }
+    }
 }
