c45cb53b5b9f32b62bbe60690d2697b42b4e4bc2
Bahadir-coder <bahadurmusayev15@gmail.com>
2026-03-19 04:54:57 +0400

    Client Layer Update

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,7 +1,15 @@
 package Client;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 public class Client {
-    Socket socket = new Socket();
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket();
+        SocketAddress socketAddress = new InetSocketAddress("127.0.0.1", 9000);
+        socket.connect(socketAddress, 60);
+        System.out.println("Client side socket created and connected");
+    }
 }
