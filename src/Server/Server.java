07971d2bb460b980ef1776163b1a66695c1a6176
Bahadir-coder <bahadurmusayev15@gmail.com>
2026-03-23 05:18:42 +0400

    Server Layer Update

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,8 +1,6 @@
 package Server;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,18 +11,13 @@ public class Server {
         ServerSocket socket = new ServerSocket();
         SocketAddress socketAddress = new InetSocketAddress("127.0.0.1", 8080);
         socket.bind(socketAddress);
-
         System.out.println("The Server Side Socket created");
-        Socket s = socket.accept();
 
-        InputStream inputStream = s.getInputStream();
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-
-        if (s != null) {
-            System.out.println(bufferedInputStream.read());
+        while(true){
+            Socket s = socket.accept();
+            InputStream inputStream = s.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            System.out.println(bufferedReader.read());
         }
-
-        bufferedInputStream.close();
-        socket.close();
     }
 }
